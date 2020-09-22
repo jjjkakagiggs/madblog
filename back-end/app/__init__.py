ff30327623fb000d70d3dc013cb120f9ef27ba24
kakagiggs <kakagiggs@126.com>
2020-09-22 11:57:57 +0800

    Flask设计User用户相关API

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from config import Config
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 cors = CORS()
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     register_blueprints(app)
-    cors.init_app(app)
+    register_plugins(app)
 
     return app
 
@@ -19,3 +23,13 @@ def create_app(config_class=Config):
 def register_blueprints(app):
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
+
+
+def register_plugins(app):
+    cors.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+
+from app import models
+
